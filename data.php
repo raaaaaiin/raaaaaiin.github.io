1a98b32f9b417f5b719856e142d4b947487473d4
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2021-10-22 10:56:42 +0800

    Initial Commit

---
+++ data.php
@@ -0,0 +1,16 @@
+<?php
+
+$navItem = array("Home", "About", "Service");
+$navTitle = "Raaaaaiin";
+$yearofExperience = 6;
+$experience = "Im a Computer and Mobile Technician, Video, Photo , 2D/3D, and Motion Graphics Editor. also has a Bachelor's Degree in Computer Science. My Specialty is Programming, Im a Full-stack Developer";
+$dataExperience = array("Front End", "Back End", "Database", "DevOps");
+$countExperience = array("50%", "75%", "45%","25%");
+$service = array( "Programming", "Editing", "Repair", "Design");
+$servicelogo = array( "fa fa-laptop-code", "fas fa-film", "fas fa-tools", "fas fa-cubes");
+$serviceText = array("Software Development  Web Development using : C, C++, C#, vb.net, Java,Python,Html, Css(Css5, Bootstrap), Javascript(Angular,Jquery), Java(Spring), and PHP.",
+ "Video, Graphic, Motion Graphic editor Using After Effects, and Premiere",
+  "I Accept Computer/Laptop , Mobile , and Electronics Repair. I also Setup Computer Network, Computer Server, Maintain and Repair Computer System",
+"I do some designing such as 2/3D Graphic, Logo Editing, T-shirt Designing, Web Designing, and Architectural Design using Photoshop, Illustrator, Blender, Maya,and Sketch up");
+
+?>
